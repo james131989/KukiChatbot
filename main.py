b3bca22c9281f435e371f4adfe286c22fa96e72a
james131989 <78683666+james131989@users.noreply.github.com>
2021-10-02 10:31:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def kukiai(client: Client, message: Message):
   msg = message.text
   chat_id = message.chat.id
 
-  Kuki =   requests.get(f"http://kuki-api.tk/api/meredith/james/message={msg}").json()
+  Kuki =   requests.get(f"https://kuki-api.tk/api/botname/owner/message={msg}").json()
 
   moezilla = f"{Kuki['reply']}"
       
