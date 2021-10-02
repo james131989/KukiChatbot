797166c5b275513d6f9eac0e075693f8d68b60bf
james131989 <78683666+james131989@users.noreply.github.com>
2021-10-02 10:40:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ kuki = Client(
 )
 
 @kuki.on_message(
-    filters.regex("Olivia|olivia|OliviaBenson|oliviabenson|benson")
-    & filters.text
+    filters.text
     & filters.reply
     & ~filters.bot
     & ~filters.edited,
