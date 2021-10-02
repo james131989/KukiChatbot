f51643a4bea2f0b73370d72a8be891dd451822f3
james131989 <78683666+james131989@users.noreply.github.com>
2021-10-02 10:20:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ async def kukiai(client: Client, message: Message):
 
   Kuki =   requests.get(f"http://kuki-api.tk/api/reply/meredith/james/female/2/language=en/message=hi").json()
 
-  james = f"{Kuki['reply']}"
+  moezilla = f"{Kuki['reply']}"
       
   await client.send_chat_action(message.chat.id, "typing")
-  await message.reply_text(james)
+  await message.reply_text(moezilla)
 
 
 messageprivate = '''
